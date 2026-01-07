496d212904ce79b1804027c4af700274c4a2501d
Abdulrazaq <abdulrazzak1835@gmail.com>
2026-01-08 01:48:32 +0530

    css changes

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,30 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.css";
-import {NavLink} from 'react-router-dom'
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handlelogout = () => {
+    try {
+      localStorage.removeItem("authToken");
+      localStorage.clear();
+    } catch (error) {}
+  };
+
   return (
-      <nav className="nav">
-      
-        <div className="heading">
-            <h2>Quiz Karo</h2>
-          </div>    
-          <div className="btn-box">
+    <nav className="nav">
+      <div className="heading">
+        <h2>Quiz Karo</h2>
+      </div>
+      <div className="btn-box">
+        <NavLink
+          className={({ isActive }) =>
+            isActive ? "result-btn active" : "result-btn"
+          }
+          to={"/result"}
+        >
+          Result
+        </NavLink>
 
-         <NavLink className={({isActive})=> isActive ? "result-btn active": "result-btn"} to={'/result'}>
+        {loggedIn ? (
+          <button onClick={handlelogout} className="login-btn">
+            Loguot
+          </button>
+        ) : (
+          <NavLink to={"/login"} className="login-btn">
+            Login
+          </NavLink>
+        )}
+      </div>
 
-
-         Result</NavLink>
-
-      <NavLink className={({isActive})=> isActive ? "login-btn active": "login-btn"} to={'/result'}>
-
-
-         LogIn</NavLink>
-
-        </div>
-       
-      </nav>
- 
+      <div>
+        
+      </div>
+    </nav>
   );
 };
 
